2beff1545183192d75689f21ecf449ea4c053eba
mathias labora martirena <mathiaslabora@gmail.com>
2022-02-23 14:52:19 -0300

    final video 2

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,8 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoController {
@@ -10,17 +9,30 @@ public class TodoController {
     @Autowired
     private TodoService service;
 
-    @GetMapping(value=)
+    @GetMapping(value="api/todos")
     public Iterable<Todo> list(){
         return service.list();
     }
-    public Todo save(Todo todo){
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo){
         return service.save(todo);
     }
-    public void delete(Long id){
+
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId() != null) {
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualizar");
+
+    }
+
+    @DeleteMapping(value="api/{id}todo")
+    public void delete(@PathVariable("id")Long id){
         service.delete(id);
     }
-    public Todo get(Long id){
+    @GetMapping(value="api/{id}todo")
+    public Todo get(@PathVariable("id") Long id){
         return service.get(id);
     }
 
