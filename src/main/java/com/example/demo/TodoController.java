1d54fe5abfceb3fb97fa04fbdb1f4fdaefc33b5a
mathias labora martirena <mathiaslabora@gmail.com>
2022-02-23 16:51:04 -0300

    finalizando

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3001")
 public class TodoController {
 
     @Autowired
